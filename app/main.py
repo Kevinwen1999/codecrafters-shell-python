2f0ea8a742cdf1c3829d2ab8a05829c181a0614c
Kevinwen1999 <cybergodzilla1999@gmail.com>
2025-01-12 17:01:34 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ import sys
 
 def main():
     
-    
+    built_in_list = ["echo", "exit", "type"]
 
     # Wait for user input
     while True:
@@ -13,6 +13,13 @@ def main():
             break
         elif (user_input[0:5] == "echo "):
             print(f"{user_input[5:]}")
+            continue
+        elif (user_input[0:5] == "type "):
+            arg = user_input[5:]
+            if (arg in built_in_list):
+                print(f"{arg} is a shell builtin")
+            else:
+                print(f"{arg}: not found")
             continue
         print(f"{user_input}: command not found")
 
