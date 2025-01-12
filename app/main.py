1d3eeef94f5d265aa8f1cdb8c11a052e388e1653
Kevinwen1999 <cybergodzilla1999@gmail.com>
2025-01-12 16:40:31 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,11 +3,13 @@ import sys
 
 def main():
     
-    sys.stdout.write("$ ")
+    
 
     # Wait for user input
-    user_input = input()
-    print(f"{user_input}: command not found")
+    while True:
+        sys.stdout.write("$ ")
+        user_input = input()
+        print(f"{user_input}: command not found")
 
 
 if __name__ == "__main__":
