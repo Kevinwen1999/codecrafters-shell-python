ca06220ac592e1c7f99a99431e2a71fb82446170
Kevinwen1999 <cybergodzilla1999@gmail.com>
2025-01-12 17:14:05 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,4 +1,17 @@
 import sys
+import os
+
+def find_program_in_path(program_name):
+    # Get the PATH environment variable
+    path_dirs = os.environ.get("PATH", "").split(os.pathsep)
+    
+    for directory in path_dirs:
+        # Construct the full path of the program
+        program_path = os.path.join(directory, program_name)
+        if os.path.isfile(program_path) and os.access(program_path, os.X_OK):
+            return program_path  # Return the first found executable path
+    
+    return None  # Return None if the program is not found
 
 
 def main():
@@ -18,6 +31,8 @@ def main():
             arg = user_input[5:]
             if (arg in built_in_list):
                 print(f"{arg} is a shell builtin")
+            elif (find_rst := find_program_in_path(arg)):
+                print(f"{arg} is {find_rst}")
             else:
                 print(f"{arg}: not found")
             continue
